b8a1a32866638fe8c84a27c064aee492b1db8c73
austin <aclyde11@gmail.com>
2019-03-03 16:16:39 -0600

    commit

--- model.py
+++ model.py
@@ -189,7 +189,7 @@ class VAE_CNN(nn.Module):
         self.fc_bn4 = nn.BatchNorm1d(rep_size)
 
         # Decoder
-        self.conv15 = nn.ConvTranspose2d(self.rep_size, 256, kernel_size=2, stride=2, padding=1, output_padding=1, bias=False)
+        self.conv15 = nn.ConvTranspose2d(300, 256, kernel_size=2, stride=2, padding=1, output_padding=1, bias=False)
         self.conv15_ = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2, padding=1, bias=False)
         self.bn15 = nn.BatchNorm2d(128)
         self.conv16 = nn.ConvTranspose2d(128, 32, kernel_size=3, stride=2, padding=1, bias=False)
