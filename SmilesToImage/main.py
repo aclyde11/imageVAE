f210f2793785f665bf5495af309da6c298d2a338
austin <aclyde11@gmail.com>
2019-03-06 10:43:17 -0800

    commit

--- SmilesToImage/main.py
+++ SmilesToImage/main.py
@@ -47,18 +47,17 @@ class customLoss(nn.Module):
     def __init__(self):
         super(customLoss, self).__init__()
         self.mse_loss = nn.MSELoss(reduction="sum")
-        self.crispyLoss = MS_SSIM()
 
     def forward(self, x_recon, x, mu, logvar, epoch):
         loss_MSE = self.mse_loss(x_recon, x)
         loss_KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         loss_cripsy = self.crispyLoss(x_recon, x)
 
-        return 1.25 * loss_MSE + min(1.0, float(round(epochs / 2 + 0.75)) * KLD_annealing) * loss_KLD + 0.9 * loss_cripsy
+        return 1.25 * loss_MSE + min(1.0, float(round(epochs / 2 + 0.75)) * KLD_annealing) * loss_KLD
 
 model = None
 if model_load is None:
-    model = VAE_CNN()
+    model = SmilesToImageModle()
 else:
     model = torch.load(model_load)
 if load_state is not None:
