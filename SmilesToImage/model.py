db9415369b0d8326b72af2d3ce748efac96de344
austin <aclyde11@gmail.com>
2019-03-07 15:45:59 -0800

    commit

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -85,22 +85,24 @@ class PictureDecoder(nn.Module):
         out = self.relu(self.conv15_(out))
         out = self.bn15(out)
         out = self.relu(self.conv16(out))
-        ouu = self.relu(self.conv16_(out))
+        out = self.relu(self.conv16_(out))
         out = self.bn16(out)
+        print(out.shape)
 
         out = self.relu(self.conv20(out))
         out = self.relu(self.conv20_(out))
         out = self.bn20(out)
-
+        print(out.shape)
         out = self.relu(self.conv17(out))
         out = self.relu(self.conv17_(out))
         out = self.bn21(out)
+        print(out.shape)
 
         out = self.relu(self.conv18(out))
         out = self.relu(self.conv18_(out))
         out = self.bn22(out)
         out = self.conv19(out)
-
+        print(out.shape)
         return out
 
 
