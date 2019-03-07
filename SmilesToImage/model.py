6a2e55768bcadc3821a83ba9d82b22d8122a79d3
austin <aclyde11@gmail.com>
2019-03-07 15:40:40 -0800

    commit

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -40,6 +40,7 @@ class SmilesEncoder(nn.Module):
         x = self.relu(self.conv4(x))
         x = self.relu(self.conv5(x))
         x = self.relu(self.conv6(x))
+        print(x.shape)
         x = x.view(-1, 256 * 2)
         x = self.relu(self.fc_bn1(self.fc1(x)))
 
