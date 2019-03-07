3aa2f07f1cc4004bdb5ee2fb16ddffcb8f4b8c03
austin <aclyde11@gmail.com>
2019-03-07 13:05:00 -0800

    commit

--- SmilesToImage/main.py
+++ SmilesToImage/main.py
@@ -98,7 +98,7 @@ val_losses = []
 train_losses = []
 
 def get_batch_size(epoch):
-    return min(64 * epoch, 256 * 12)
+    return min(32 * epoch, 256 * 12)
 
 def train(epoch):
     train_loader_food = generate_data_loader(train_root, get_batch_size(3), int(rampDataSize * data_size))
