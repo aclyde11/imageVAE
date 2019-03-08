a6a99cfb9d44b85b9e8e04c87bad192936884aff
austin <aclyde11@gmail.com>
2019-03-08 13:18:56 -0600

    commit

--- SmilesToImage/train_image_image.py
+++ SmilesToImage/train_image_image.py
@@ -74,7 +74,7 @@ class customLoss(nn.Module):
         loss_KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         loss_cripsy = self.crispyLoss(x_recon, x)
 
-        return loss_MSE + min(1.0, float(round(epochs / 2 + 0.75)) * KLD_annealing) * loss_KLD + 0 * loss_cripsy
+        return loss_MSE + min(1.0, float(round(epochs / 2 + 0.75)) * KLD_annealing) * loss_KLD + 0.2 * loss_cripsy
 
 model = None
 encoder = None
