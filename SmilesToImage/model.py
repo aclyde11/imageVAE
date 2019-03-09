5f72142c8a4940f08e0ddbbc9a0c238e55845b7b
austin <aclyde11@gmail.com>
2019-03-09 11:53:23 -0600

    commit

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -74,7 +74,7 @@ class SmilesEncoder(nn.Module):
         ##layers
 
         self.conv1 = nn.Conv1d(in_channels=self.max_length_sequence, out_channels=9, kernel_size=9, stride=1)
-        self.conv2 = nn.Conv1d(in_channels=9, out_channels=9, kernel_size=9, stride=1)
+        self.conv2 = nn.Conv1d(in_channels=9, out_channels=9, kernel_size=10, stride=1)
         self.conv3 = nn.Conv1d(in_channels=9, out_channels=10, kernel_size=9, stride=1)
 
         self.relu = nn.ReLU()
