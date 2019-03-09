a38a8fc78bf307dc4214c44cd95e51934529dbed
austin <aclyde11@gmail.com>
2019-03-09 17:52:47 -0600

    commit

--- SmilesToImage/sample_image_image.py
+++ SmilesToImage/sample_image_image.py
@@ -108,25 +108,25 @@ def sample(epoch, model, data):
         images = []
         n_samples_linspace = 16
         for i in range(n_image_gen):
-            data_latent = model.module.encode_latent_(data)
+            data_latent = model.encode_latent_(data)
             pt_1 = data_latent[i * 2, ...].cpu().numpy()
             pt_2 = data_latent[i * 2 + 1, ...].cpu().numpy()
             sample_vec = interpolate_points(pt_1, pt_2, np.linspace(0, 1, num=n_samples_linspace, endpoint=True))
             sample_vec = torch.from_numpy(sample_vec).to(device)
-            images.append(model.module.decode(sample_vec).cpu())
+            images.append(model.decode(sample_vec).cpu())
         save_image(torch.cat(images), output_dir + 'linspace_' + str(epoch) + '.png', nrow=n_samples_linspace)
 
         n_image_gen = 8
         images = []
         n_samples_linspace = 16
         for i in range(n_image_gen):
-            data_latent = model.module.encode_latent_(data)
+            data_latent = model.encode_latent_(data)
             pt_1 = data_latent[i, ...].cpu().numpy()
             pt_2 = data_latent[i + 1, ...].cpu().numpy()
             sample_vec = interpolate_points(pt_1, pt_2,
                                             np.linspace(0, 1, num=n_samples_linspace, endpoint=True))
             sample_vec = torch.from_numpy(sample_vec).to(device)
-            images.append(model.module.decode(sample_vec).cpu())
+            images.append(model.decode(sample_vec).cpu())
         save_image(torch.cat(images), output_dir + 'linspace_path_' + str(epoch) + '.png', nrow=n_samples_linspace)
 
 
