3139dc53cb5dccdbe4192003fdab57711451a2b4
austin <aclyde11@gmail.com>
2019-03-09 12:04:08 -0600

    commit

--- SmilesToImage/train_smiles_smiles.py
+++ SmilesToImage/train_smiles_smiles.py
@@ -68,14 +68,12 @@ class customLoss(nn.Module):
     def __init__(self):
         super(customLoss, self).__init__()
         #self.mse_loss = nn.MSELoss(reduction="sum")
-        self.crispyLoss = MS_SSIM()
 
     def forward(self, x_recon, x, mu, logvar, epoch):
         loss_MSE = torch.nn.functional.binary_cross_entropy(x_recon, x)
         loss_KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        loss_cripsy = self.crispyLoss(x_recon, x)
 
-        return loss_MSE + min(1.0, float(round(epochs / 2 + 0.75)) * KLD_annealing) * loss_KLD +  loss_cripsy
+        return loss_MSE + min(1.0, float(round(epochs / 2 + 0.75)) * KLD_annealing) * loss_KLD
 
 model = None
 encoder = None
