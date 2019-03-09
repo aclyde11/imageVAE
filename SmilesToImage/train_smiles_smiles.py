c5222c21ce5e11bbf3da8c6ef6de622fa702d18c
austin <aclyde11@gmail.com>
2019-03-09 13:17:05 -0600

    commit

--- SmilesToImage/train_smiles_smiles.py
+++ SmilesToImage/train_smiles_smiles.py
@@ -19,7 +19,7 @@ seed = 42
 data_para = True
 log_interval = 25
 LR = 0.001           ##adam rate
-rampDataSize = 0.33 ## data set size to use
+rampDataSize = 0.1 ## data set size to use
 embedding_width = 60
 vocab = pickle.load( open( "/homes/aclyde11/moldata/charset.p", "rb" ) )
 print("VOCAB: ", vocab)
