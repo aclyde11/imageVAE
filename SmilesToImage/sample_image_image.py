e98e96b549799ad61b39b57d01fa18bd27604a91
austin <aclyde11@gmail.com>
2019-03-09 18:43:22 -0600

    commit

--- SmilesToImage/sample_image_image.py
+++ SmilesToImage/sample_image_image.py
@@ -148,8 +148,8 @@ def imscatter(x, y, ax, imageData, zoom):
         x0, y0 = x[i], y[i]
         # Convert to image
         img = imageData[i] * 255.
-        img = img.astype(np.uint8).reshape([256, 256])
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        img = img.astype(np.uint8).reshape([3, 256, 256])
+        #img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         # Note: OpenCV uses BGR and plt uses RGB
         image = OffsetImage(img, zoom=zoom)
         ab = AnnotationBbox(image, (x0, y0), xycoords='data', frameon=False)
