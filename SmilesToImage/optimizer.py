758c5fc0ddd396f09071dcf27152e5ed73d8956a
austin <aclyde11@gmail.com>
2019-03-12 15:26:41 -0500

    commit

--- SmilesToImage/optimizer.py
+++ SmilesToImage/optimizer.py
@@ -57,13 +57,14 @@ class ImageFolderWithFile(datasets.ImageFolder):
     def __getitem__(self, index):
         t = self.imgs[index][0]
         t = int(t.split('/')[-1].split('.')[0])
+        i = t
         try:
             t = list(smiles_lookup.iloc[t-1, 1])
         except:
             print(t)
             exit()
         embed = apply_one_hot([t])[0].astype(np.float32)
-        im = super(ImageFolderWithFile, self).__getitem__(index), t-1
+        im = super(ImageFolderWithFile, self).__getitem__(index), i-1
 
         return  im, embed
 
