3fc13399c0323d075cfadfbb40e0dd9dc6354144
austin <aclyde11@gmail.com>
2019-03-12 11:13:25 -0500

    commit

--- SmilesToImage/train_smiles_images.py
+++ SmilesToImage/train_smiles_images.py
@@ -132,15 +132,15 @@ optimizer = optim.Adam(encoder.parameters(), lr=LR)
 #optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.8, nesterov=True)
 #sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10, eta_min=0.000001, last_epoch=-1)
 
-train_loader = generate_data_loader(train_root, 300, int(50000))
-val_loader = generate_data_loader(val_root, 300, int(10000))
+train_loader = generate_data_loader(train_root, 350, int(50000))
+val_loader = generate_data_loader(val_root, 350, int(10000))
 
 
 val_losses = []
 train_losses = []
 
 def get_batch_size(epoch):
-    return 300
+    return 350
 
 def train(epoch):
 
