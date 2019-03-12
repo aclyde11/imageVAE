d5839b8e5ea2f281e12ed39b10d6ca6036be6321
austin <aclyde11@gmail.com>
2019-03-11 20:27:48 -0500

    commit

--- SmilesToImage/train_image_image.py
+++ SmilesToImage/train_image_image.py
@@ -16,7 +16,7 @@ from invert import Invert
 
 import numpy as np
 import pandas as pd
-starting_epoch=127
+starting_epoch=129
 epochs = 200
 no_cuda = False
 seed = 42
@@ -29,7 +29,7 @@ vocab = pickle.load( open( "/homes/aclyde11/moldata/charset.p", "rb" ) )
 embedding_size = len(vocab)
 KLD_annealing = 0.05  ##set to 1 if not wanted.
 #load_state = None
-model_load = {'decoder' : '/homes/aclyde11/imageVAE/im_im_small/model/decoder_epoch_127.pt', 'encoder':'/homes/aclyde11/imageVAE/im_im_small/model/encoder_epoch_127.pt'}
+model_load = {'decoder' : '/homes/aclyde11/imageVAE/im_im_small/model/decoder_epoch_129.pt', 'encoder':'/homes/aclyde11/imageVAE/im_im_small/model/encoder_epoch_129.pt'}
 cuda = True
 data_size = 1400000
 torch.manual_seed(seed)
