c0545244c3a5564e4ae2db3578cb3314d7495a5a
austin <aclyde11@gmail.com>
2019-03-13 15:49:47 -0500

    Revert "commit"
    This reverts commit a5d79938

--- SmilesToImage/train_combo_learning.py
+++ SmilesToImage/train_combo_learning.py
@@ -39,8 +39,8 @@ model_load = {'decoder' : '/homes/aclyde11/imageVAE/im_im_small/model/decoder_ep
 cuda = True
 data_size = 1400000
 torch.manual_seed(seed)
-output_dir = '/homes/aclyde11/imageVAE/smi_im/results/'
-save_files = '/homes/aclyde11/imageVAE/smi_im/model/'
+output_dir = '/homes/aclyde11/imageVAE/combo/results/'
+save_files = '/homes/aclyde11/imageVAE/combo/model/'
 device = torch.device("cuda" if cuda else "cpu")
 kwargs = {'num_workers': 12, 'pin_memory': True} if cuda else {}
 
@@ -136,15 +136,15 @@ optimizer = optim.Adam(model.parameters(), lr=LR)
 #optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.8, nesterov=True)
 #sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10, eta_min=0.000001, last_epoch=-1)
 
-train_loader = generate_data_loader(train_root, 225, int(2000))
-val_loader = generate_data_loader(val_root, 225, int(2000))
+train_loader = generate_data_loader(train_root, 400, int(80000))
+val_loader = generate_data_loader(val_root, 400, int(10000))
 mse = customLoss()
 
 val_losses = []
 train_losses = []
 
 def get_batch_size(epoch):
-    return 225
+    return 400
 
 def train(epoch):
 
