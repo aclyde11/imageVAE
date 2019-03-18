a4ac0570309e0015fd5c2a73f04045301dae0f9a
austin <aclyde11@gmail.com>
2019-03-18 18:20:05 -0500

    Revert "Revert "commit""
    This reverts commit 306d7af050077c3e9b92c8263ad0a8de205697f3.

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -579,7 +579,7 @@ class AutoModel(nn.Module):
 
     def __init__(self, encoder, decoder):
         super(AutoModel, self).__init__()
-        self.encoder = ResNet(Bottleneck, [3, 4, 23, 3], num_class=500)
+        self.encoder = ResNet(Bottleneck, [3, 4, 23, 3], num_classes=500)
         self.attention = nn.Linear(500, 500)
         self.reduce = nn.Linear(500, 292)
         self.decoder = decoder
