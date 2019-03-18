536c4540b7cee6e1f1715ae1a5c793c346753b01
austin <aclyde11@gmail.com>
2019-03-18 12:38:47 -0500

    Revert "Revert "commit""
    This reverts commit 306d7af050077c3e9b92c8263ad0a8de205697f3.

--- SmilesToImage/train_combo_learning.py
+++ SmilesToImage/train_combo_learning.py
@@ -134,7 +134,7 @@ optimizer = optim.Adam(model.parameters(), lr=LR)
 sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 4, eta_min=0.000001, last_epoch=-1)
 
 train_loader = generate_data_loader(train_root, 1400, int(50000))
-val_loader = generate_data_loader(val_root, 100, int(1000))
+val_loader = generate_data_loader(val_root, 100, int(800))
 
 val_losses = []
 train_losses = []
