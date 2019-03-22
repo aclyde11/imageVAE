99513938d55849cb71e3d0a7c64d331148369454
austin <aclyde11@gmail.com>
2019-03-22 12:26:55 -0500

    Revert "Revert "commit""
    This reverts commit 306d7af050077c3e9b92c8263ad0a8de205697f3.

---
+++ Staker/models.py
@@ -0,0 +1,250 @@
+import torch
+from torch.autograd import Variable
+from skimage import io, transform
+from torch import nn, optim
+from torch.nn import functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def conv5x5(in_planes, out_planes, stride=1):
+    """5x5 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=5, stride=stride,
+                     padding=2, bias=False)
+
+
+class BasicBlock(nn.Module):
+
+    def __init__(self, inplanes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv5x5(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv5x5(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.stride = stride
+        if self.stride > 1:
+            self.maxpooling = nn.MaxPool2d(kernel_size=self.stride, stride=1, padding=0, dilation=1, ceil_mode=False)
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.stride > 1:
+            out = self.maxpooling(out)
+            identity   = self.maxpooling(identity)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+    def get_output_size(self, input_dim):
+        if self.stride == 1:
+            return input_dim
+        else:
+            return (input_dim + 2 * 0 - 1 * (2 - 1) - 1) / 1 + 1
+
+
+class Encoder(nn.Module):
+    def __init__(self, params):
+        super(Encoder, self).__init__()
+        params = {
+            'block1_layers' : 2,
+            'block2_layers' : 4,
+            'block4_layers' : 3
+        }
+
+        self.params = params
+
+        self.block1 = self.__make__blocks(params['block1_layers'], pooling=1, inplanes=3, outplanes=3)
+        self.block2 = self.__make__blocks(params['block2_layers'], pooling=2, inplanes=3, outplanes=3)
+        self.block3 = self.__make__blocks(1, pooling=1, inplanes=3, outplanes=3)
+        self.block4 = self.__make__blocks(params['block4_layers'], pooling=2, inplanes=3, outplanes=3)
+        self.statefc = nn.Sequential(nn.Linear(250, 250), nn.ReLU())
+
+        self.gridLSTM = None
+        self.attention = None
+
+
+    def __make__blocks(self, number_blocks, pooling=1, inplanes=3, outplanes=3):
+        layers = []
+        for i in range(number_blocks):
+            layers.append(BasicBlock(inplanes, outplanes, pooling))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.block1(x)
+        out = self.block2(out)
+        atten_out = self.block3(out)
+        out = self.block4(atten_out)
+
+        state_vector = self.statefc(out.view(-1, 256 * 256))
+
+
+        return state_vector
+
+class Attention(nn.Module):
+    """
+    Attention Network.
+    """
+
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
+        """
+        :param encoder_dim: feature size of encoded images
+        :param decoder_dim: size of decoder's RNN
+        :param attention_dim: size of the attention network
+        """
+        super(Attention, self).__init__()
+        self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
+        self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
+        self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
+
+    def forward(self, encoder_out, decoder_hidden):
+        """
+        Forward propagation.
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+        :param decoder_hidden: previous decoder output, a tensor of dimension (batch_size, decoder_dim)
+        :return: attention weighted encoding, weights
+        """
+        att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
+        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
+        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        alpha = self.softmax(att)  # (batch_size, num_pixels)
+        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
+
+        return attention_weighted_encoding, alpha
+
+
+
+class DecoderWithAttention(nn.Module):
+    """
+    Decoder.
+    """
+
+    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, encoder_dim=2048, dropout=0.5):
+        """
+        :param attention_dim: size of attention network
+        :param embed_dim: embedding size
+        :param decoder_dim: size of decoder's RNN
+        :param vocab_size: size of vocabulary
+        :param encoder_dim: feature size of encoded images
+        :param dropout: dropout
+        """
+        super(DecoderWithAttention, self).__init__()
+
+        self.encoder_dim = encoder_dim
+        self.attention_dim = attention_dim
+        self.embed_dim = embed_dim
+        self.decoder_dim = decoder_dim
+        self.vocab_size = vocab_size
+        self.dropout = dropout
+
+        self.attention = Attention(encoder_dim, decoder_dim, attention_dim)  # attention network
+
+        self.embedding = nn.Embedding(vocab_size, embed_dim)  # embedding layer
+        self.dropout = nn.Dropout(p=self.dropout)
+        self.decode_step = nn.LSTMCell(embed_dim + encoder_dim, decoder_dim, bias=True)  # decoding LSTMCell
+        self.init_h = nn.Linear(encoder_dim, decoder_dim)  # linear layer to find initial hidden state of LSTMCell
+        self.init_c = nn.Linear(encoder_dim, decoder_dim)  # linear layer to find initial cell state of LSTMCell
+        self.f_beta = nn.Linear(decoder_dim, encoder_dim)  # linear layer to create a sigmoid-activated gate
+        self.sigmoid = nn.Sigmoid()
+        self.fc = nn.Linear(decoder_dim, vocab_size)  # linear layer to find scores over vocabulary
+        self.init_weights()  # initialize some layers with the uniform distribution
+
+    def init_weights(self):
+        """
+        Initializes some parameters with values from the uniform distribution, for easier convergence.
+        """
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+        self.fc.bias.data.fill_(0)
+        self.fc.weight.data.uniform_(-0.1, 0.1)
+
+    def load_pretrained_embeddings(self, embeddings):
+        """
+        Loads embedding layer with pre-trained embeddings.
+        :param embeddings: pre-trained embeddings
+        """
+        self.embedding.weight = nn.Parameter(embeddings)
+
+    def fine_tune_embeddings(self, fine_tune=True):
+        """
+        Allow fine-tuning of embedding layer? (Only makes sense to not-allow if using pre-trained embeddings).
+        :param fine_tune: Allow?
+        """
+        for p in self.embedding.parameters():
+            p.requires_grad = fine_tune
+
+    def init_hidden_state(self, encoder_out):
+        """
+        Creates the initial hidden and cell states for the decoder's LSTM based on the encoded images.
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+        :return: hidden state, cell state
+        """
+        mean_encoder_out = encoder_out.mean(dim=1)
+        h = self.init_h(mean_encoder_out)  # (batch_size, decoder_dim)
+        c = self.init_c(mean_encoder_out)
+        return h, c
+
+    def forward(self, encoder_out, encoded_captions, caption_lengths):
+        """
+        Forward propagation.
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, enc_image_size, enc_image_size, encoder_dim)
+        :param encoded_captions: encoded captions, a tensor of dimension (batch_size, max_caption_length)
+        :param caption_lengths: caption lengths, a tensor of dimension (batch_size, 1)
+        :return: scores for vocabulary, sorted encoded captions, decode lengths, weights, sort indices
+        """
+
+        batch_size = encoder_out.size(0)
+        encoder_dim = encoder_out.size(-1)
+        vocab_size = self.vocab_size
+
+        # Flatten image
+        encoder_out = encoder_out.view(batch_size, -1, encoder_dim)  # (batch_size, num_pixels, encoder_dim)
+        num_pixels = encoder_out.size(1)
+
+        # Sort input data by decreasing lengths; why? apparent below
+        caption_lengths, sort_ind = caption_lengths.squeeze(1).sort(dim=0, descending=True)
+        encoder_out = encoder_out[sort_ind]
+        encoded_captions = encoded_captions[sort_ind]
+
+        # Embedding
+        embeddings = self.embedding(encoded_captions)  # (batch_size, max_caption_length, embed_dim)
+
+        # Initialize LSTM state
+        h, c = self.init_hidden_state(encoder_out)  # (batch_size, decoder_dim)
+
+        # We won't decode at the <end> position, since we've finished generating as soon as we generate <end>
+        # So, decoding lengths are actual lengths - 1
+        decode_lengths = (caption_lengths - 1).tolist()
+
+        # Create tensors to hold word predicion scores and alphas
+        predictions = torch.zeros(batch_size, max(decode_lengths), vocab_size).to(device)
+        alphas = torch.zeros(batch_size, max(decode_lengths), num_pixels).to(device)
+
+        # At each time-step, decode by
+        # attention-weighing the encoder's output based on the decoder's previous hidden state output
+        # then generate a new word in the decoder with the previous word and the attention weighted encoding
+        for t in range(max(decode_lengths)):
+            batch_size_t = sum([l > t for l in decode_lengths])
+            attention_weighted_encoding, alpha = self.attention(encoder_out[:batch_size_t],
+                                                                h[:batch_size_t])
+            gate = self.sigmoid(self.f_beta(h[:batch_size_t]))  # gating scalar, (batch_size_t, encoder_dim)
+            attention_weighted_encoding = gate * attention_weighted_encoding
+            h, c = self.decode_step(
+                torch.cat([embeddings[:batch_size_t, t, :], attention_weighted_encoding], dim=1),
+                (h[:batch_size_t], c[:batch_size_t]))  # (batch_size_t, decoder_dim)
+            preds = self.fc(self.dropout(h))  # (batch_size_t, vocab_size)
+            predictions[:batch_size_t, t, :] = preds
+            alphas[:batch_size_t, t, :] = alpha
+
+        return predictions, encoded_captions, decode_lengths, alphas, sort_ind
---
+++ Staker/train.py
@@ -0,0 +1,276 @@
+from comet_ml import Experiment
+from torch.nn.utils.rnn import pack_padded_sequence
+
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1, 2, 3, 4,5,6,7'
+from itertools import chain
+
+import datetime
+import torch
+from torch import nn, optim
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+import torchvision
+import pickle
+from PIL import  ImageOps
+from models import DecoderWithAttention, Encoder
+from invert import Invert
+
+import numpy as np
+import pandas as pd
+
+
+hyper_params = {
+    "num_epochs": 1000,
+    "train_batch_size": 28,
+    "val_batch_size": 128,
+    'seed' : 42,
+    "learning_rate": 0.001
+}
+
+
+experiment = Experiment(project_name="pytorch")
+experiment.log_parameters(hyper_params)
+
+starting_epoch=1
+epochs = hyper_params['num_epochs']
+no_cuda = False
+seed = hyper_params['seed']
+data_para = True
+log_interval = 7
+LR = hyper_params['learning_rate']       ##adam rate
+rampDataSize = 0.2 ## data set size to use
+embedding_width = 60
+vocab = pickle.load( open( "/homes/aclyde11/moldata/charset.p", "rb" ) )
+vocab.insert(0,' ')
+print(vocab)
+embedding_width = 60
+embedding_size = len(vocab)
+embedding_size = len(vocab)
+KLD_annealing = 0.05  ##set to 1 if not wanted.
+#load_state = None
+model_load1 = {'decoder' : '/homes/aclyde11/imageVAE/combo/model/decoder1_epoch_111.pt', 'encoder':'/homes/aclyde11/imageVAE/smi_smi/model/encoder_epoch_100.pt'}
+#model_load = None
+cuda = True
+data_size = 1400000
+torch.manual_seed(seed)
+output_dir = '/homes/aclyde11/imageVAE/combo/results/'
+save_files = '/homes/aclyde11/imageVAE/combo/model/'
+device = torch.device("cuda" if cuda else "cpu")
+kwargs = {'num_workers': 16, 'pin_memory': True} if cuda else {}
+
+
+train_root = '/homes/aclyde11/moldata/moses/train/'
+val_root =   '/homes/aclyde11/moldata/moses/test/'
+smiles_lookup = pd.read_table("/homes/aclyde11/moldata/moses_cleaned.tab", header=None)
+
+def from_one_hot_array(vec):
+    oh = np.where(vec == 1)
+    if oh[0].shape == (0, ):
+        return None
+    return int(oh[0][0])
+
+def decode_smiles_from_indexes(vec, charset):
+    return "".join(map(lambda x: charset[x], vec)).strip()
+
+def one_hot_array(i, n):
+    return map(int, [ix == i for ix in range(n)])
+
+def one_hot_index(vec, charset):
+    return map(charset.index, vec)
+one_hot_encoded_fn = lambda row: np.array(map(lambda x: one_hot_array(x, len(vocab)),
+                                     one_hot_index(row, vocab)))
+def apply_t(x):
+    x = x + list((''.join([char*(embedding_width - len(x)) for char in [' ']])))
+    smi = one_hot_encoded_fn(x)
+    return smi
+
+def apply_one_hot(ch):
+    return np.array(map(apply_t, ch))
+
+class ImageFolderWithFile(datasets.ImageFolder):
+    def __getitem__(self, index):
+        t = self.imgs[index][0]
+        t = int(t.split('/')[-1].split('.')[0])
+        t = list(smiles_lookup.iloc[t, 1])
+        embed = apply_one_hot([t])[0].astype(np.float32)
+        return  super(ImageFolderWithFile, self).__getitem__(index), embed, len(embed)
+
+def generate_data_loader(root, batch_size, data_size):
+    invert = transforms.Compose([
+        Invert(),
+        transforms.ToTensor()
+    ])
+    return torch.utils.data.DataLoader(
+        ImageFolderWithFile(root, transform=invert),
+        batch_size=batch_size, shuffle=False, drop_last=True, sampler=torch.utils.data.SubsetRandomSampler(list(range(0, data_size))),  **kwargs)
+
+def clip_gradient(optimizer, grad_clip):
+    """
+    Clips gradients computed during backpropagation to avoid explosion of gradients.
+    :param optimizer: optimizer with the gradients to be clipped
+    :param grad_clip: clip value
+    """
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
+
+emb_dim = 60  # dimension of word embeddings
+attention_dim = 512  # dimension of attention linear layers
+decoder_dim = 512  # dimension of decoder RNN
+dropout = 0.5
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
+encoder_lr = 1e-4  # learning rate for encoder if fine-tuning
+decoder_lr = 4e-4  # learning rate for decoder
+grad_clip = 5.  # clip gradients at an absolute value of
+alpha_c = 1.  # regularization parameter for 'doubly stochastic attention', as in the paper
+fine_tune_encoder = False  # fine-tune encoder?
+decoder = DecoderWithAttention(attention_dim=attention_dim,
+                               embed_dim=emb_dim,
+                               decoder_dim=decoder_dim,
+                               vocab_size=len(embedding_size),
+                               dropout=dropout)
+
+decoder_optimizer = torch.optim.Adam(params=filter(lambda p: p.requires_grad, decoder.parameters()),
+                                     lr=decoder_lr)
+encoder = Encoder()
+encoder_optimizer = torch.optim.Adam(params=filter(lambda p: p.requires_grad, encoder.parameters()),
+                                     lr=encoder_lr) if fine_tune_encoder else None
+
+
+
+train_loader = generate_data_loader(train_root, 500, int(50000))
+val_loader = generate_data_loader(val_root, 100, int(800))
+criterion = nn.CrossEntropyLoss().to(device)
+
+class AverageMeter(object):
+    """
+    Keeps track of most recent, average, sum, and count of a metric.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def get_batch_size(epoch):
+    return 100
+
+def train(epoch):
+    with experiment.train():
+        experiment.log_current_epoch(epoch)
+
+        print("Epoch {}: batch_size {}".format(epoch, get_batch_size(epoch)))
+        decoder.train()  # train mode (dropout and batchnorm is used)
+        encoder.train()
+        losses = AverageMeter()  # loss (per word decoded)
+        for batch_idx, (data, embed, embedlen) in enumerate(train_loader):
+            data = data[0].float().cuda()
+            caps = embed.float().cuda()
+            caplens = embedlen.float().cuda()
+
+            # Forward prop.
+            imgs = encoder(imgs)
+            scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(imgs, caps, caplens)
+
+            # Since we decoded starting with <start>, the targets are all words after <start>, up to <end>
+            targets = caps_sorted[:, 1:]
+
+            # Remove timesteps that we didn't decode at, or are pads
+            # pack_padded_sequence is an easy trick to do this
+            scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
+            targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
+
+            # Calculate loss
+            loss = criterion(scores, targets)
+
+            # Add doubly stochastic attention regularization
+            loss += alpha_c * ((1. - alphas.sum(dim=1)) ** 2).mean()
+
+            # Back prop.
+            decoder_optimizer.zero_grad()
+            if encoder_optimizer is not None:
+                encoder_optimizer.zero_grad()
+            loss.backward()
+
+            # Clip gradients
+            if grad_clip is not None:
+                clip_gradient(decoder_optimizer, grad_clip)
+                if encoder_optimizer is not None:
+                    clip_gradient(encoder_optimizer, grad_clip)
+
+            # Update weights
+            decoder_optimizer.step()
+            if encoder_optimizer is not None:
+                encoder_optimizer.step()
+
+            # Keep track of metrics
+            losses.update(loss.item(), sum(decode_lengths))
+
+            if batch_idx % log_interval == 0:
+
+
+
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} {}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                           100. * batch_idx / len(train_loader),
+                           loss.item() / len(data), datetime.datetime.now()))
+
+        print('====> Epoch: {} Average loss: {:.4f}'.format(
+            epoch, loss.avg / len(train_loader.dataset)))
+
+
+
+
+def test(epoch):
+    with experiment.test():
+        experiment.log_current_epoch(epoch)
+        model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for i, (data, embed, smiles) in enumerate(val_loader):
+                data = data[0].float().cuda()
+                embed = embed.float().cuda()
+                recon_batch = model(data)
+
+                loss = lossf(recon_batch.float(), embed)
+
+                test_loss += loss.item()
+
+                if i == 0:
+                    for i in range(4):
+                        sampled = recon_batch.cpu().detach().numpy()[i, ...].argmax(axis=1)
+                        mol = embed.cpu().numpy()[i, ...].argmax(axis=1)
+                        mol = decode_smiles_from_indexes(mol, vocab)
+                        sampled = decode_smiles_from_indexes(sampled, vocab)
+                        print("Orig: ", mol, " Sample: ", sampled, ' BCE: ')
+
+    experiment.log_metric("loss", test_loss)
+    test_loss /= len(val_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+    val_losses.append(test_loss)
+
+for epoch in range(starting_epoch, epochs):
+
+
+    if epoch > 100:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.0001
+    for param_group in optimizer.param_groups:
+        print("Current learning rate is: {}".format(param_group['lr']))
+    train(epoch)
+    test(epoch)
+
