dc23190d7c44e69898485570b72a4a56e3c2cc0a
austin <aclyde11@gmail.com>
2019-03-28 10:55:00 -0500

    updated staker

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -357,7 +357,7 @@ class GeneralVae(nn.Module):
 
     def reparameterize(self, mu, logvar):
         if self.training:
-            std = logvar.mul(0.5).exp_()
+            std = torch.exp(logvar.mul(0.5))
             eps = Variable(std.data.new(std.size()).normal_())
             return eps.mul(std).add_(mu)
         else:
