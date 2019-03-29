7f3fca112e5ff8865e8ba5799bbbd7cd00afe7f9
austin <aclyde11@gmail.com>
2019-03-29 08:30:38 -0500

    updated staker

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -248,11 +248,65 @@ class ListModule(nn.Module):
 #             concats = torch.cat((concats, x), dim=1)
 #         x = self.final_layer(concats)
 #         return x
+def attention(q, k, v, d_k, mask=None, dropout=None):
+    scores = torch.matmul(q, k.transpose(-2, -1)) / torch.sqrt(d_k)
+
+    if mask is not None:
+        mask = mask.unsqueeze(1)
+        scores = scores.masked_fill(mask == 0, -1e9)
+    scores = F.softmax(scores, dim=-1)
+
+    if dropout is not None:
+        scores = dropout(scores)
+
+    output = torch.matmul(scores, v)
+    return output
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, heads, d_model, dropout=0.1):
+        super().__init__()
+
+        self.d_model = d_model
+        self.d_k = d_model // heads
+        self.h = heads
+
+        self.q_linear = nn.Linear(d_model, d_model)
+        self.v_linear = nn.Linear(d_model, d_model)
+        self.k_linear = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.out = nn.Linear(d_model, d_model)
+
+    def forward(self, q, k, v, mask=None):
+        bs = q.size(0)
+
+        # perform linear operation and split into h heads
+
+        k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
+        q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
+        v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
+
+        # transpose to get dimensions bs * h * sl * d_model
+
+        k = k.transpose(1, 2)
+        q = q.transpose(1, 2)
+        v = v.transpose(1, 2)
+        # calculate attention using function we will define next
+        scores = attention(q, k, v, self.d_k, mask, self.dropout)
+
+        # concatenate heads and put through final linear layer
+        concat = scores.transpose(1, 2).contiguous().view(bs, -1, self.d_model)
+
+        output = self.out(concat)
+
+        return output
+
 class BindingAffModel(nn.Module):
     def __init__(self, rep_size=500, dropout=None):
         super(BindingAffModel, self).__init__()
         self.rep_size = rep_size
 
+
+        self.attention = MultiHeadAttention(1, 500)
         self.model = nn.Sequential(
             nn.Linear(500, 128),
             nn.SELU(),
@@ -267,7 +321,9 @@ class BindingAffModel(nn.Module):
 
 
     def forward(self, x):
-        return self.model(x)
+        batch_size=x.shape[0]
+        out = self.attention(x.view(-1, 1, self.rep_size), x.view(-1, 1, self.rep_size), x.view(-1, 1, self.rep_size))
+        return self.model(out.view(batch_size, -1))
 
 class PictureDecoder(nn.Module):
     def __init__(self, rep_size=500):
