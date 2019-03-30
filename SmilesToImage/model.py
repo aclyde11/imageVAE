82d82b05582dc454050d9c7083c9072c6e42da61
austin <aclyde11@gmail.com>
2019-03-30 09:27:45 -0500

    updated staker

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -361,7 +361,7 @@ class PictureDecoder(nn.Module):
         self.conv4 = TranposeConvBlock(64,  64, kernel_size=[4, 4], stride=[1, 2], padding=[1, 1])
         self.conv5 = TranposeConvBlock(64,  64, kernel_size=[4, 4], stride=[1, 1], padding=[1, 1])
         self.conv6 = TranposeConvBlock(64, 32, kernel_size=[5, 40], stride=[1, 2], padding=[0,0])
-        self.conv7 = TranposeConvBlock(32, 16, kernel_size=[40, 40], stride=[1, 2], padding=[0,0])
+        self.conv7 = TranposeConvBlock(32, 16, kernel_size=[40, 40], stride=[1, 1], padding=[0,0])
         self.conv8 = TranposeConvBlock(16, 3, kernel_size=[40, 40], stride=[1, 1], padding=[0,0])
         self.conv9 = TranposeConvBlock(3, 3, kernel_size=[20, 20], stride=[1, 1,], padding=[0,0])
         self.conv10 = TranposeConvBlock(3, 3, kernel_size=[20, 20], stride=[1, 1,], padding=[0,0])
