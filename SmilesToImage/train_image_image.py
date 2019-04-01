abb177c48ead133828d34ce94d82395246abd6ce
austin <aclyde11@gmail.com>
2019-04-01 09:10:11 -0500

    updated staker

--- SmilesToImage/train_image_image.py
+++ SmilesToImage/train_image_image.py
@@ -11,9 +11,14 @@ import pickle
 from PIL import  ImageOps
 from utils import MS_SSIM
 from invert import Invert
-
+from rdkit import Chem
+from rdkit.Chem import rdDepictor
+from rdkit.Chem.Draw import rdMolDraw2D
 import numpy as np
 import pandas as pd
+from PIL import Image
+import io
+import cairosvg
 starting_epoch=1
 epochs = 200
 no_cuda = False
@@ -77,13 +82,43 @@ class ImageFolderWithFile(datasets.ImageFolder):
 
         return  im, 0, aff
 
+class MoleLoader(torch.utils.data.Dataset):
+    def __init__(self, df, subsample):
+        super(MoleLoader, self).__init__()
+        self.df = df
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def make_image(self,mol, molSize=(256, 256), kekulize=True, mol_name=''):
+        mc = Chem.Mol(mol.ToBinary())
+        if kekulize:
+            try:
+                Chem.Kekulize(mc)
+            except:
+                mc = Chem.Mol(mol.ToBinary())
+        if not mc.GetNumConformers():
+            rdDepictor.Compute2DCoords(mc)
+        drawer = rdMolDraw2D.MolDraw2DSVG(molSize[0], molSize[1])
+        drawer.DrawMolecule(mc)
+        drawer.FinishDrawing()
+        svg = drawer.GetDrawingText()
+        image = Image.open(io.BytesIO(cairosvg.svg2png(bytestring=svg,  parent_width=100, parent_height=100,
+                         scale=1)))
+        image.convert('RGB')
+        return Invert(image)
+
+    def __getitem__(self, item):
+        smile = self.df.iloc[item, 0]
+        image = self.make_image(smile)
+
+        return smile, transforms.ToTensor(image)
+
+
+
 def generate_data_loader(root, batch_size, data_size):
-    invert = transforms.Compose([
-        Invert(),
-        transforms.ToTensor()
-    ])
     return torch.utils.data.DataLoader(
-        ImageFolderWithFile(root, transform=invert),
+        MoleLoader(smiles_lookup),
         batch_size=batch_size, shuffle=False, drop_last=True, sampler=torch.utils.data.SubsetRandomSampler(list(range(0, data_size))),  **kwargs)
 
 
