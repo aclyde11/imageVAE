082d4c2675cf6a461efed33bde3ac9369b661d71
austin <aclyde11@gmail.com>
2019-04-02 15:36:35 -0500

    Added combo

--- ImageModel/invert.py
+++ ImageModel/invert.py
@@ -28,13 +28,15 @@ def adjust_learning_rate(args, optimizer, epoch, step, len_epoch):
     # if epoch >= 80:
 
         #     factor = factor + 1
-    lr = None
-    if epoch > 5 and epoch % 10 == 0:
+    lr = args.lr
+    if epoch > 5 and epoch % 20 == 0:
         lr = args.lr * 0.66
 
-    """Warmup"""
-    if epoch < 5:
-        lr = lr * float(1 + step + epoch * len_epoch) / (5. * len_epoch)
+    lr = max(1e-6, lr)
+
+    # """Warmup"""
+    # if epoch < 5:
+    #     lr = lr * float(1 + step + epoch * len_epoch) / (5. * len_epoch)
 
     # if(args.local_rank == 0):
     #     print("epoch = {}, step = {}, lr = {}".format(epoch, step, lr))
