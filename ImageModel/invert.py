a966a8c31c1c3b5b35304f06ae10d435ac695ee3
austin <aclyde11@gmail.com>
2019-04-02 18:09:38 -0500

    Revert "updated staker"
    This reverts commit 7f43ab5c

--- ImageModel/invert.py
+++ ImageModel/invert.py
@@ -23,23 +23,20 @@ def interpolate_points(x, y, sampling):
 
 def adjust_learning_rate(args, optimizer, epoch, step, len_epoch):
     """LR schedule that should yield 76% converged accuracy with batch size 256"""
-    # factor = epoch // 30
-    #
-    # if epoch >= 80:
-    #     factor = factor + 1
-    #
-    # lr = args.lr * (0.1 ** factor)
-    #
-    # """Warmup"""
-    # if epoch < 5:
-    #     lr = lr * float(1 + step + epoch * len_epoch) / (5. * len_epoch)
-    #
-    # # if(args.local_rank == 0):
-    # #     print("epoch = {}, step = {}, lr = {}".format(epoch, step, lr))
-    lr = args.lr
-    if epoch < 100 and epoch > 5 and epoch % 10 == 0 and step == 0:
-        print("Adjsuted lr: {}".format(lr))
-        lr = lr * 0.66
+    factor = epoch // 30
+
+    if epoch >= 80:
+        factor = factor + 1
+
+    lr = args.lr * (0.1 ** factor)
+
+    """Warmup"""
+    if epoch < 5:
+        lr = lr * float(1 + step + epoch * len_epoch) / (5. * len_epoch)
+
+    if(args.local_rank == 0):
+        print("epoch = {}, step = {}, lr = {}".format(epoch, step, lr))
+
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
