b69a84bf38c90f80d7b60f08fe04e73aade0c940
austin <aclyde11@gmail.com>
2019-04-02 18:42:17 -0500

    Merge branch 'old-state'
    # Conflicts:
    #	ImageModel/DataLoader.py

--- SmilesToImage/train_image_image.py
+++ SmilesToImage/train_image_image.py
@@ -102,14 +102,14 @@ class customLoss(nn.Module):
     def __init__(self):
         super(customLoss, self).__init__()
         self.mse_loss = nn.MSELoss(reduction="mean")
-        self.crispyLoss = MS_SSIM()
+        #self.crispyLoss = MS_SSIM()
 
     def forward(self, x_recon, x, mu, logvar, epoch):
         loss_MSE = self.mse_loss(x_recon, x)
         loss_KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        loss_cripsy = self.crispyLoss(x_recon, x)
+        #loss_cripsy = self.crispyLoss(x_recon, x)
 
-        return loss_MSE + min(1.0, float(round(epochs / 2 + 0.75)) * KLD_annealing) * loss_KLD +  loss_cripsy
+        return loss_MSE + min(1.0, float(round(epochs / 2 + 0.75)) * KLD_annealing) * loss_KLD
 
 model = None
 encoder = None
