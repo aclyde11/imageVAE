3cd316905af759b84a001e4d75bb96a5798e5302
austin <aclyde11@gmail.com>
2019-04-07 19:55:42 -0500

    updating models

--- GridLSTM/DataLoader.py
+++ GridLSTM/DataLoader.py
@@ -66,7 +66,7 @@ class MoleLoader(torch.utils.data.Dataset):
         return map(int, [ix == i for ix in range(n)])
 
     def one_hot_index(self, vec, charset):
-        return map(list(charset), vec)
+        return map(lambda x : charset[x], vec)
 
     def one_hot_encoded_fn(self, row):
         return np.array(list(map(lambda x: self.one_hot_array(x, self.vocab), self.one_hot_index(row, self.vocab))))
