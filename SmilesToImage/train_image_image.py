3e513d2a1b19c78ea702ac4553b328ea262dcbb6
austin <aclyde11@gmail.com>
2019-04-12 06:38:34 -0500

    updating models

--- SmilesToImage/train_image_image.py
+++ SmilesToImage/train_image_image.py
@@ -116,7 +116,7 @@ decoder = None
 encoder = PictureEncoder()
 decoder = PictureDecoder()
 
-checkpoint = torch.load( output_dir + 'sample_' + str(14) + '.png', map_location='cpu')
+checkpoint = torch.load( save_files + 'epoch_' + str(14) + '.pt', map_location='cpu')
 encoder.load_state_dict(checkpoint['encoder_state_dict'])
 decoder.load_state_dict(checkpoint['decoder_state_dict'])
 
