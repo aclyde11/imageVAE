8f7b47caab5cfd1458c4fccfbb3099db094a6441
austin <aclyde11@gmail.com>
2019-04-16 17:45:31 -0500

    updating models

--- SmilesToImage/open_ball_sample.py
+++ SmilesToImage/open_ball_sample.py
@@ -258,8 +258,8 @@ def test(epoch):
                 # loss = loss_picture(recon_batch, data, mu, logvar, epoch)
                 # loss = torch.sum(loss)
 
-                experiment.log_metric('loss', loss.item())
-                test_loss += loss.item()
+                # experiment.log_metric('loss', loss.item())
+                # test_loss += loss.item()
                 if i == 0:
                     ##
 
