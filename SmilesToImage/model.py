f090999f0e28a8b3e5bf99728dc3641ed7377f5f
austin <aclyde11@gmail.com>
2019-04-18 11:43:49 -0500

    updating models

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -700,7 +700,7 @@ class PictureDecoder(nn.Module):
         # Decoder
         self.preconv = nn.ConvTranspose2d(4, 4, kernel_size=3, stride=2, padding=0, bias=False)
         self.conv15 = nn.ConvTranspose2d(4, 4, kernel_size=3, stride=2, padding=0, bias=False)
-        self.conv15_ = nn.ConvTranspose2d(3, 3, kernel_size=3, stride=2, padding=0, bias=False)
+        self.conv15_ = nn.ConvTranspose2d(4, 3, kernel_size=3, stride=2, padding=0, bias=False)
         self.upper = nn.UpsamplingBilinear2d(size=(128,128))
         self.bn15 = nn.BatchNorm2d(3)
         self.conv16 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1, bias=False)
