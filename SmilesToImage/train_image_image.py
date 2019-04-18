0a2079216deb05bbf56f50c0cab7684abc9bbe10
austin <aclyde11@gmail.com>
2019-04-18 11:38:47 -0500

    updating models

--- SmilesToImage/train_image_image.py
+++ SmilesToImage/train_image_image.py
@@ -108,7 +108,7 @@ class customLoss(nn.Module):
         loss_KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         loss_cripsy = self.crispyLoss(x_recon, x)
 
-        return loss_MSE + loss_KLD + 10000.0 * loss_cripsy
+        return loss_MSE + loss_KLD + loss_cripsy *(256 * 256)
 
 model = None
 encoder = None
