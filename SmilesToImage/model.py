0b8927399a6e176afd351c831d9a8ba5145d072e
austin <aclyde11@gmail.com>
2019-04-18 09:09:54 -0500

    updating models

--- SmilesToImage/model.py
+++ SmilesToImage/model.py
@@ -214,7 +214,7 @@ class PixelCNNLayer_down(nn.Module):
 
 
 class PixelCNN(nn.Module):
-    def __init__(self, nr_resnet=5, nr_filters=80, nr_logistic_mix=10,
+    def __init__(self, nr_resnet=5, nr_filters=10, nr_logistic_mix=10,
                  resnet_nonlinearity='concat_elu', input_channels=3):
         super(PixelCNN, self).__init__()
         if resnet_nonlinearity == 'concat_elu':
